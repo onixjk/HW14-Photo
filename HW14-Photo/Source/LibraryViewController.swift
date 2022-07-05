8d516c3d8723a121be700b7aa72b42f731a312a8
onixjk <onixjkk@gmail.com>
2022-07-05 18:26:53 +0300

    Add LibraryViewController
    Add LibraryViewController in NavigationController. Add NavigationController in tabBar. Add Metric. Setup view.

---
+++ HW14-Photo/Source/LibraryViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LibraryViewController.swift
+//  HW14-Photo
+//
+//  Created by Onix qq on 5.07.22.
+//
+
+import UIKit
+
+class LibraryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+    }
+    
+    func setupView() {
+        view.backgroundColor = .white
+        title = Strings.LibraryNavigationControllerTitle
+    }
+    
+}
---
+++ HW14-Photo/Source/Metric.swift
@@ -0,0 +1,10 @@
+//
+//  Metric.swift
+//  HW14-Photo
+//
+//  Created by Onix qq on 5.07.22.
+//
+
+enum Strings {
+    static let LibraryNavigationControllerTitle = "Медиатека"
+}
