36280fa79b8a943efb7a4f800b95dc2103e0bcf5
onixjk <onixjkk@gmail.com>
2022-07-05 18:48:25 +0300

    Setup ForYouViewController.
    Add forYouViewController in forYouNavigationController. Add forYouNavigationController in tabBar. Setup View.

---
+++ HW14-Photo/Source/ForYouViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ForYouViewController.swift
+//  HW14-Photo
+//
+//  Created by Onix qq on 5.07.22.
+//
+
+import UIKit
+
+class ForYouViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupView()
+    }
+    
+    func setupView() {
+        view.backgroundColor = .white
+        title = Strings.ForYouNavigationControllerTitle
+    }
+}
